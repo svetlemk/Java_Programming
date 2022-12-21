63a1cc57481fdcd96fb6513c40d6c65e55d62b06
skocherry <svetlemk@gmail.com>
2022-12-21 08:30:50 -0600

    added phone class

---
+++ src/day30_inheritance/phone_task/Phone.java
@@ -0,0 +1,121 @@
+package day30_inheritance.phone_task;
+
+import java.rmi.ServerError;
+import java.util.Arrays;
+
+public class Phone {
+    private String brand;
+    private String model, size;
+    private double price;
+    private String color;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price <= 0) {
+            System.err.println("The price is invalid: " + price);
+            System.exit(1);
+        }
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        String[] colors = {"Black", "White", "Silver", "Gold", "Pink"};
+        if (Arrays.asList(colors).contains(color)) {
+            this.color = color;
+        } else {
+            System.err.println("Invalid color: " + color + " phone can only be  " + Arrays.asList(colors));
+            System.exit(1);
+        }
+
+    }
+
+    public Phone(String brand, String model, String size, double price, String color) {
+        setBrand(brand);
+        setModel(model);
+        setSize(size);
+        setPrice(price);
+        setColor(color);
+    }
+
+    public void call(long phoneNumber){
+        System.out.println(getModel() + " is calling "+ phoneNumber);}
+
+    public void text(long phoneNumber){
+        System.out.println(getModel() + " is texting " + phoneNumber);
+    }
+
+    public String toString() {
+        return getClass().getSimpleName()+"Phone{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", size='" + size + '\'' +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
+/*1. Create a named Phone:
+				Variables:
+					brand, model, size, price, color
+
+				Encapsulate all the fields
+					Conditions:
+						1. price can not be zero or negative
+						2. color can only be set to:
+								{"Black", "White", "Silver", "Gold", "Pink"}
+
+				Add a constructor that can set all the fields
+
+				Methods:
+					call(long phoneNumber)
+					text(long phoneNumber)
+					toString()
+
+	2. Create the following sub classes of Phone:
+
+			2.1 Iphone:
+					Extra methods:
+						faceTime(phoneNumber)
+						faceTile(email)
+
+			2.2 Samsung:
+					Extra methods:
+						freeze()
+
+			2.3 Nokia:
+					Extra methods:
+						selfDefense()
+
+ */
